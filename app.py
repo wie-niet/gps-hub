69480c28071c41f92e30ac850c6181dc48deb882
Diederik Hamelink <diederik@wie-niet.nl>
2019-11-14 14:36:07 +0100

    Flask web app.py added

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask, jsonify, make_response
+import gpshublib
+
+# Creating Flask application
+app = Flask(__name__)
+app.debug = True
+
+
+#
+# API /user
+#
+from RestApi import DeviceHardwareRestApi
+
+
+gps_hw_view = DeviceHardwareRestApi.as_view('gps_hw_api')
+app.add_url_rule('/gps_hw/', defaults={'id': None}, view_func=gps_hw_view, methods=['GET',])
+app.add_url_rule('/gps_hw/', view_func=gps_hw_view, methods=['POST',])
+app.add_url_rule('/gps_hw/<int:id>', view_func=gps_hw_view, methods=['GET', 'PUT', 'DELETE', 'PATCH'])
+
+
+
+if __name__ == '__main__':
+    app.run()
+    
