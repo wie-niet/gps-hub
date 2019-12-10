dae3957a87df464105c36bb6f436dec200ed368d
Diederik Hamelink <diederik@wie-niet.nl>
2019-12-10 12:50:39 +0100

    gpx_files

--- app.py
+++ app.py
@@ -3,6 +3,11 @@ from flask import Flask, jsonify, make_response
 import gpshublib
 import toml_config_table
 
+# global data stores
+ds = {}
+ds['conf_list'] = toml_config_table.GpsConfigCollection()
+ds['dev_hw_list'] = gpshublib.DeviceHardwareList()
+
 
 
 # Creating Flask application
@@ -17,7 +22,7 @@ CORS(app)
 #
 # API /gps_dev_view
 #
-gps_dev_view = gpshublib.DeviceHardwareRestApi.as_view('gps_dev_api')
+gps_dev_view = gpshublib.DeviceHardwareRestApi(dev_hw_list=ds['dev_hw_list']).as_view('gps_dev_api')
 app.add_url_rule('/gps_dev/', defaults={'id': None}, view_func=gps_dev_view, methods=['GET',])
 app.add_url_rule('/gps_dev/', view_func=gps_dev_view, methods=['POST',])
 app.add_url_rule('/gps_dev/<id>', view_func=gps_dev_view, methods=['GET', 'PUT', 'DELETE', 'PATCH'])
@@ -27,10 +32,23 @@ app.add_url_rule('/gps_dev/<id>', view_func=gps_dev_view, methods=['GET', 'PUT',
 #
 # API /gps_hw
 #
-gps_conf_view = toml_config_table.DeviceConfigRestApi.as_view('gps_conf_api')
+gps_conf_view = toml_config_table.DeviceConfigRestApi(conf_list=ds['conf_list']).as_view('gps_conf_api')
 app.add_url_rule('/gps_conf/', defaults={'id': None}, view_func=gps_conf_view, methods=['GET',])
 app.add_url_rule('/gps_conf/', view_func=gps_conf_view, methods=['POST',])
 app.add_url_rule('/gps_conf/<id>', view_func=gps_conf_view, methods=['GET', 'PUT', 'DELETE', 'PATCH'])
+
+# #
+# # GPX files meta data /gps/<id>/files/
+# #
+# gpx_files_view = *******.DeviceConfigRestApi.as_view('gpx_files_api')
+# app.add_url_rule('/gps/<gps_id>/files/', defaults={'id': None}, view_func=gpx_files_view, methods=['GET',])
+# # app.add_url_rule('/gps/<gps_id>/files/', view_func=gpx_files_view, methods=['POST',])
+# app.add_url_rule('/gps/<gps_id>/files/<id>', view_func=gpx_files_view, methods=['GET', 'PUT', 'DELETE', 'PATCH'])
+
+#
+# GPX files meta data /gps/<id>/files_data/
+#
+
 
 
 if __name__ == '__main__':
---
+++ gpx_files.py
@@ -0,0 +1,81 @@
+from os import listdir
+from os.path import isfile, join
+
+from rest_api_flask import RestApi, ApiItem, ApiList, request
+from rest_api_jsonschema import JsonSchemaForRestApi
+
+
+class GpxFile(ApiItem):
+	def __init__(self, filename):
+		self.id = filename
+
+		
+
+
+#
+# RestApi 
+#
+class GpxFilesRestApi( JsonSchemaForRestApi, RestApi):
+	
+	def __init__(self, data_store):
+		self.ds  = data_store
+
+	def magic_parse_route(self, **kwargs):
+		## ROUTE : /gps/<string:gps_id>/files/<string:id> 
+		self.gps_id = kwargs.get('gps_id')
+		
+
+
+	def get_gpx_path(self):
+		gps_id = self.gps_id
+		
+		# get GPS config dict
+		gps_conf = self.ds['conf_list'].first(gps_id)
+
+		# get gpshublib.DeviceHardware object (for is_mounted) 
+		gps_hw = self.ds['dev_hw_list'].find(gps_id)
+		
+		# check if gps device is mounted
+		if not gps_hw.sys_is_mounted:
+			error = {'error': 'GPS Device not mounted'}
+			self.response(error, 404)
+		
+		# return gpx_path
+		return(join(gps_hw.sys_is_mountpoint, gps_conf['gpx_dir']))
+		
+
+	def db_find_one(self, filename):
+		# find 1 in data layer
+		gpx_path = self.get_gpx_path()
+
+		# test if it's a file
+		if not isfile(join(gpx_path, filename)):
+			error = {'error': 'GPX file not found.'}
+			self.response(error, 404)
+		
+		# get all GPX File ApiItem 
+		gpx_file = GpxFile(filename)
+		
+		# return item
+		return(gpx_file)
+		
+
+	def db_list(self):
+		# get list from data layer
+		gpx_path = self.get_gpx_path()
+		
+		# list of filenames
+		onlyfiles = [f for f in listdir(gpx_path) if isfile(join(gpx_path, f))]
+		
+		data = []
+		for filename in onlyfiles:
+			data.append(self.GpxFile(filename))
+		
+		
+		return(data)
+		
+		
+	def db_delete(self, id):
+		# delete from data layer
+		raise NotImplementedError('implement db_delete in your own class')
+
